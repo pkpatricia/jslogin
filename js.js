ddf1f18a280c50c4cdde3b65b3713ef03283c006
Patricia Parker <pk.patricia@gmail.com>
2019-09-30 19:40:38 -0500

    changed form

--- js.js
+++ js.js
@@ -9,37 +9,21 @@ localStorage.clear();
 
 */
 
-document.querySelector('form').addEventListener('submit', function(e){
+let userid = form.userid.value;
+let password = form.password.value;
 
-    var myuserid = document.getElementById('userid').value;
-    var mypassword = document.getElementById('password').value;
-
-    //const task = document.getElementById('task').value;
-  
-    //let tasks;
-
-    //const myuserid = JSON.parse(localStorage.getItem('myuserid'));
-  
-  
-    if(localStorage.getItem('tasks') === null) {
-      tasks = [];
-    } else {
-      isLoggedIn = JSON.parse(localStorage.getItem('userid'));
-    }
-  
-    //tasks.push(task);
-
-    if(myuserid)
-  
-    localStorage.setItem('isLoggedIn', 'YES');
-  
-    alert('Password Correct');
-  
-    e.preventDefault();
-  });
-  
-  /* const tasks = JSON.parse(localStorage.getItem('tasks'));
-  
-  tasks.forEach(function(task){
-    console.log(task);
-  }); */
+function check(form)
+{
+ /*the following code checkes whether the entered userid and password are matching*/
+ if (userid == 'myuserid' && password == 'mypassword')
+  {
+    window.open('myPage.html');
+    localStorage.setItem('userid', 'myuserid');
+    localStorage.setItem('password', 'mypassword');
+    alert('Welcome!' + localStorage.getItem('userid'));
+  }
+ else
+ {
+   alert("Error Password or Username");
+  }
+}
